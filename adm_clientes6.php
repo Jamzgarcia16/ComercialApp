9eca521e15b43e245a8c323d6b18cb1abb33173b
jamzgarcia <jamzgarcia@misena.edu.co>
2020-04-03 21:56:35 -0500

    commit de prueba pc back

--- adm_clientes6.php
+++ adm_clientes6.php
@@ -4,4 +4,5 @@ defined("TOKEN_L34567") or die("Acceso no autorizado!");
 $sql="SELECT * FROM clientes ORDER BY id_cliente";
 $tabla=$db->sub_tuplas($sql);
 $p1->assign("tabla_clientes",$tabla);
+//$p1->assign("tabla_clientes",$tabla);
 ?>
