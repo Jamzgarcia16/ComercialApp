07c3e9b9cdadd1e5853a569be921ae293c9045ea
Jamzgarcia16 <jamzgarcia@misena.edu.co>
2020-04-30 00:46:13 -0500

    vue con simulador api json ok peticion ajax ok en index.html 2 login ok

--- js/CAPP/01.js
+++ js/CAPP/01.js
@@ -1,37 +1,18 @@
-const app = new Vue({
-  
-  el: '#app',
-  data: {
-    titulo: 'GYM con Vue',
-    tareas: [],
-    nuevaTarea: ''
-  },
-  methods:{
-    agregarTarea: function(){
-      this.tareas.push({
-        nombre: this.nuevaTarea,
-        estado: false
-      });
-      this.nuevaTarea = '';
-      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
-    },
-    editarTarea: function(index){
-      this.tareas[index].estado = true;
-      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
-    },
-    eliminar: function(index){
-      this.tareas.splice(index, 1);
-      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
-    }
-  },
-  created: function(){
-    let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
-    if(datosDB === null){
-      this.tareas = [];
-    }else{
-      this.tareas = datosDB;
-    }
-  }
-
-
-});
+var urlUsers = 'https://randomuser.me/api/?results=1';
+//		var urlUsers = 'https://jsonplaceholder.typicode.com/users';
+		new Vue({
+			el: '#main',
+			created: function() {
+				this.getUsers();
+			},
+			data: {
+				lists: []
+			},
+			methods: {
+				getUsers: function() {
+					this.$http.get(urlUsers).then(function(response){
+						this.lists = response.data.results;
+					});
+				}
+			}
+		});
